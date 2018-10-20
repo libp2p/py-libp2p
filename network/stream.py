48dfd9a77ad3b5534c9d4a4beed0dd1fd36300c8
Alex Haynes <alhaynes@seas.upenn.edu>
2018-10-20 13:42:16 -0400

    started on stream

--- network/stream.py
+++ network/stream.py
@@ -1,4 +1,5 @@
 from .stream_interface import IStream
+import asyncio
 
 class Stream(IStream):
 
@@ -6,18 +7,30 @@ class Stream(IStream):
         self.context = context
         self.peer_id = peer_id
 
+        peer_store = context.peer_store
+        peer_addr = peer_store.get(peer_id)
+
+        # look up peer_id -> multiaddr in peer store
+        # parse multiaddr and set_protocol based on it
+        # open connection to multiaddr
+        # save connection to stream's state
+        self.open_connection(ip, port)
+
+    async def open_connection(self, ip, port):
+        self.reader, self.writer = await asyncio.open_connection(ip, port)
+
     def protocol(self):
         """
         :return: protocol id that stream runs on
         """
-        pass
+        return self.protocol_id
 
     def set_protocol(self, protocol_id):
         """
         :param protocol_id: protocol id that stream runs on
         :return: true if successful
         """
-        pass
+        self.protocol_id = protocol_id
 
     def read(self):
         """
