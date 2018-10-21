f6d7ba774f5cd2b6670004d3278fb83decfb4353
Stuckinaboot <aspyn@seas.upenn.edu>
2018-10-21 14:05:39 -0400

    Add second test

---
+++ tests/test_example.py
@@ -0,0 +1,17 @@
+import pytest
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("3+5", 8),
+    ("2+4", 6),
+    ("6*9", 42),
+])
+def test_eval(test_input, expected):
+    assert eval(test_input) == expected
+
+@pytest.mark.parametrize("test_input,expected", [
+    ("3+5", 8),
+    ("2+4", 6),
+    ("6*5", 30),
+])
+def test_eval2(test_input, expected):
+    assert eval(test_input) == expected
