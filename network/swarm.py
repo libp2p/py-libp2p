a2a3e3349770cab1f2e12c0e6ca1ce293580e0e7
zixuanzh <zixuanzh@seas.upenn.edu>
2018-10-21 14:19:49 -0400

    missing listen

--- network/swarm.py
+++ network/swarm.py
@@ -1,21 +1,32 @@
 from .network_interface import INetwork
+from stream import Stream
 
 class Swarm(INetwork):
 
     def __init__(self, my_peer_id, peer_store):
         self.my_peer_id = my_peer_id
         self.peer_store = peer_store
+        self.stream_handlers = {}
 
-    def set_stream_handler(self, stream_handler):
+    def set_stream_handler(self, protocol_id, stream_handler):
         """
         :param stream_handler: a stream handler instance
         :return: true if successful
         """
-        pass
+        self.stream_handlers[protocol_id] = stream_handler
+        return True
 
     def new_stream(self, peer_id):
         """
         :param peer_id: peer_id of destination
         :return: stream instance
+        """
+        stream = Stream(peer_id)
+        return stream
+
+    def listen(self, *args):
+        """
+        :param *args: one or many multiaddrs to start listening on
+        :return: true if at least one success
         """
         pass
