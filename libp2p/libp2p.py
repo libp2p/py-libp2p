af6784546ed907e3f43f8409b17c251ac0dcc0b0
zixuanzh <zixuanzh@seas.upenn.edu>
2018-11-11 10:43:03 -0500

    entry point wip

--- libp2p/libp2p.py
+++ libp2p/libp2p.py
@@ -1,5 +1,8 @@
 from .config import Config
 from ..peer.peerstore import PeerStore
+from ..network.swarm import Swarm
+from ..host.basic_host import BasicHost
+from ..transport.upgrader import TransportUpgrader
 
 class Libp2p(object):
 
@@ -7,11 +10,26 @@ class Libp2p(object):
         transportOpt = ["/ip4/0.0.0.0/tcp/0"], \
         muxerOpt = ["mplex/6.7.0"], \
         secOpt = ["secio"], \
-        peerstoreOpt = new PeerStore()):
+        peerstore = PeerStore()):
         
         if idOpt:
             self.idOpt = idOpt
         else:
             # TODO generate RSA public key pair
 
-        # TODO initialize 
+        self.transportOpt = transportOpt
+        self.muxerOpt =  muxerOpt
+        self.secOpt = secOpt
+        self.peerstore = peerstore
+
+    def new_node(self):
+
+        swarm = Swarm(self.id, self.peerstore)
+        host = BasicHost(swarm)
+        upgrader = TransportUpgrader(self.secOpt, self.transportOpt)
+
+        # TODO transport upgrade
+
+        # TODO listen on addrs
+
+        # TODO swarm add transports
