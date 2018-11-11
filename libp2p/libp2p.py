e20d6df667c33a9f2bd91bc973e71fc7e654f5d9
Alex Haynes <alhaynes@seas.upenn.edu>
2018-11-11 13:21:47 -0500

    added rsa public key generation

--- libp2p/libp2p.py
+++ libp2p/libp2p.py
@@ -3,6 +3,7 @@ from ..peer.peerstore import PeerStore
 from ..network.swarm import Swarm
 from ..host.basic_host import BasicHost
 from ..transport.upgrader import TransportUpgrader
+from Crypto.PublicKey import RSA
 
 class Libp2p(object):
 
@@ -16,7 +17,10 @@ class Libp2p(object):
             self.idOpt = idOpt
         else:
             # TODO generate RSA public key pair
-
+            new_key = RSA.generate(2048, e=65537)
+            self.idOpt = new_key.publickey().exportKey("PEM")
+            self.private_key = new_key.exportKey("PEM")
+            
         self.transportOpt = transportOpt
         self.muxerOpt =  muxerOpt
         self.secOpt = secOpt
@@ -24,7 +28,7 @@ class Libp2p(object):
 
     def new_node(self):
 
-        swarm = Swarm(self.id, self.peerstore)
+        swarm = Swarm(self.idOpt, self.peerstore)
         host = BasicHost(swarm)
         upgrader = TransportUpgrader(self.secOpt, self.transportOpt)
 
