ff6fdccea423f6604f3e020a3cf2ab598d6123d0
zixuanzh <zixuanzh@seas.upenn.edu>
2018-11-25 01:45:13 -0500

    refactor libp2p

--- libp2p/libp2p.py
+++ libp2p/libp2p.py
@@ -6,32 +6,27 @@ from transport.upgrader import TransportUpgrader
 from transport.tcp.tcp import TCP
 
 
-class Libp2p():
+async def new_node(id_opt=None, transport_opt=None, \
+    muxer_opt=None, sec_opt=None, peerstore=None):
 
-    def __init__(self, id_opt=None, transport_opt=["/ip4/127.0.0.1/tcp/8001"], \
-        muxer_opt=["mplex/6.7.0"], sec_opt=["secio"], peerstore=PeerStore()):
+    if id_opt is None:
+        new_key = RSA.generate(2048, e=65537)
+        id_opt = new_key.publickey().exportKey("PEM")
+        # private_key = new_key.exportKey("PEM")
 
-        if id_opt:
-            self.id_opt = id_opt
-        else:
-            new_key = RSA.generate(2048, e=65537)
-            self.id_opt = new_key.publickey().exportKey("PEM")
-            self.private_key = new_key.exportKey("PEM")
+    transport_opt = transport_opt or ["/ip4/127.0.0.1/tcp/8001"]
+    muxer_opt = muxer_opt or ["mplex/6.7.0"]
+    sec_opt = sec_opt or ["secio"]
+    peerstore = peerstore or PeerStore()
 
-        self.transport_opt = transport_opt
-        self.muxer_opt = muxer_opt
-        self.sec_opt = sec_opt
-        self.peerstore = peerstore
+    upgrader = TransportUpgrader(sec_opt, transport_opt)
+    swarm = Swarm(id_opt, peerstore, upgrader)
+    tcp = TCP()
+    swarm.add_transport(tcp)
+    await swarm.listen(transport_opt[0])
 
-    async def new_node(self):
+    # TODO enable support for other host type
+    # TODO routing unimplemented
+    host = BasicHost(swarm)
 
-        upgrader = TransportUpgrader(self.sec_opt, self.transport_opt)
-        swarm = Swarm(self.id_opt, self.peerstore, upgrader)
-        tcp = TCP()
-        swarm.add_transport(tcp)
-        await swarm.listen(self.transport_opt[0])
-        host = BasicHost(swarm)
-
-        # TODO MuxedConnection currently contains all muxing logic (move to a Muxer)
-        # TODO routing unimplemented
-        return host
+    return host
