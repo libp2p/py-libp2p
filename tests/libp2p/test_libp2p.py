c69405e11dce50253a007d62c4d68242c1348ef7
Alex Haynes <alhaynes@seas.upenn.edu>
2018-11-26 23:14:24 -0500

    conforming to linter

--- tests/libp2p/test_libp2p.py
+++ tests/libp2p/test_libp2p.py
@@ -36,8 +36,8 @@ async def test_simple_messages():
 
 @pytest.mark.asyncio
 async def test_double_response():
-    hostA = await new_node(transport_opt=["/ip4/127.0.0.1/tcp/8002/ipfs/hostA"])
-    hostB = await new_node(transport_opt=["/ip4/127.0.0.1/tcp/8003/ipfs/hostB"])
+    host_a = await new_node(transport_opt=["/ip4/127.0.0.1/tcp/8002/ipfs/host_a"])
+    host_b = await new_node(transport_opt=["/ip4/127.0.0.1/tcp/8003/ipfs/host_b"])
 
     async def stream_handler(stream):
         while True:
@@ -52,12 +52,12 @@ async def test_double_response():
             print("sending response:" + response)
             await stream.write(response.encode())
 
-    hostB.set_stream_handler("/echo/1.0.0", stream_handler)
+    host_b.set_stream_handler("/echo/1.0.0", stream_handler)
 
     # Associate the peer with local ip address (see default parameters of Libp2p())
-    hostA.get_peerstore().add_addr("hostB", "/ip4/127.0.0.1/tcp/8003", 10)
-    print("hostA about to open stream")
-    stream = await hostA.new_stream("hostB", "/echo/1.0.0")
+    host_a.get_peerstore().add_addr("host_b", "/ip4/127.0.0.1/tcp/8003", 10)
+    print("host_a about to open stream")
+    stream = await host_a.new_stream("host_b", "/echo/1.0.0")
     messages = ["hello" + str(x) for x in range(10)]
     for message in messages:
         await stream.write(message.encode())
