a26626bbd30cca4f54fc2f0dcad8efa39a61d973
Tran Ly Vu <tranly.vu@accellion.com>
2019-01-02 23:47:51 +0800

    Fix lines that too long
    Signed-off-by: Tran Ly Vu <tranly.vu@accellion.com>

--- tests/peer/test_peerid.py
+++ tests/peer/test_peerid.py
@@ -7,104 +7,103 @@ from Crypto.PublicKey import RSA
 from peer.id import *
 
 def test_init_():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
-	peer_id = ID(random_id_string)
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
+    peer_id = ID(random_id_string)
 
-	assert peer_id._id_str == random_id_string
+    assert peer_id._id_str == random_id_string
 
 def test_no_init_value():
-	with pytest.raises(Exception) as e_info:
-		peer_id = ID()
+    with pytest.raises(Exception) as e_info:
+        peer_id = ID()
 
 def test_pretty():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
-	peer_id = ID(random_id_string)
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
+    peer_id = ID(random_id_string)
 
-	actual = peer_id.pretty()
-	expected = base58.b58encode(random_id_string).decode()
+    actual = peer_id.pretty()
+    expected = base58.b58encode(random_id_string).decode()
 
-	assert actual == expected
+    assert actual == expected
 
 def test_str_less_than_10():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(5))
-	pid = base58.b58encode(random_id_string).decode() 
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(5))
+    pid = base58.b58encode(random_id_string).decode() 
 
-	expected = "<peer.ID " + pid  + ">"
-	actual = ID(random_id_string).__str__()
+    expected = "<peer.ID " + pid  + ">"
+    actual = ID(random_id_string).__str__()
 
-	assert actual == expected
+    assert actual == expected
 
 def test_str_more_than_10():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
-	pid = base58.b58encode(random_id_string).decode()
-	part_1, part_2 = pid[:2], pid[len(pid)-6:]
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
+    pid = base58.b58encode(random_id_string).decode()
+    part_1, part_2 = pid[:2], pid[len(pid)-6:]
 
-	expected = "<peer.ID " + part_1 + "*" + part_2 + ">"
-	actual = ID(random_id_string).__str__()
+    expected = "<peer.ID " + part_1 + "*" + part_2 + ">"
+    actual = ID(random_id_string).__str__()
 
-	assert actual == expected
+    assert actual == expected
 
 def test_eq_true():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))	
-	other = ID(random_id_string)
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10)) 
+    other = ID(random_id_string)
 
-	expected = True
-	actual = ID(random_id_string).__eq__(other)
+    expected = True
+    actual = ID(random_id_string).__eq__(other)
 
-	assert actual == expected
+    assert actual == expected
 
 def test_eq_false():
-	other = ID("efgh")
+    other = ID("efgh")
 
-	expected = False
-	actual = ID("abcd").__eq__(other)
+    expected = False
+    actual = ID("abcd").__eq__(other)
 
-	assert actual == expected
+    assert actual == expected
 
 def test_hash():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
 
-	expected = hash(random_id_string)
-	actual = ID(random_id_string).__hash__()
+    expected = hash(random_id_string)
+    actual = ID(random_id_string).__hash__()
 
-	assert actual == expected
+    assert actual == expected
 
 def test_id_b58_encode():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
 
-	expected = base58.b58encode(random_id_string).decode()
-	actual = id_b58_encode(ID(random_id_string))
+    expected = base58.b58encode(random_id_string).decode()
+    actual = id_b58_encode(ID(random_id_string))
 
-	assert actual == expected
+    assert actual == expected
 
 def test_id_b58_decode():
-	combine_letters_digits = string.ascii_letters + string.digits
-	random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
+    combine_letters_digits = string.ascii_letters + string.digits
+    random_id_string = ''.join(random.SystemRandom().choice(combine_letters_digits) for _ in range(10))
 
-	expected = ID(base58.b58decode(random_id_string))
-	actual = id_b58_decode(random_id_string)
+    expected = ID(base58.b58decode(random_id_string))
+    actual = id_b58_decode(random_id_string)
 
-	assert actual == expected	
+    assert actual == expected   
 
 def test_id_from_public_key():
+    bits_list = [1024, 1280, 1536, 1536, 2048]
+    key = RSA.generate(random.choice(bits_list))
+    key_bin = key.exportKey("DER")
+    algo = multihash.Func.sha2_256
+    mh_digest = multihash.digest(key_bin, algo)
 
-	bits_list = [1024, 1280, 1536, 1536, 2048]
-	key = RSA.generate(random.choice(bits_list))
-	key_bin = key.exportKey("DER")
-	algo = multihash.Func.sha2_256
-	mh_digest = multihash.digest(key_bin, algo)
+    expected = ID(mh_digest.encode())
+    actual = id_from_public_key(key)
 
-	expected = ID(mh_digest.encode())
-	actual = id_from_public_key(key)
-
-	assert actual == expected
+    assert actual == expected
 
 def test_id_from_private_key():
     key = RSA.generate(2048, e=65537)
