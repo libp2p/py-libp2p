3f81bf4a21f119b4d9d830fbd60fa27dee969df8
Hyungsuk Kang <hskang9@gmail.com>
2019-02-25 11:55:36 +0900

    Update tcp.py

--- libp2p/transport/tcp/tcp.py
+++ libp2p/transport/tcp/tcp.py
@@ -68,7 +68,7 @@ class TCP(ITransport):
         """
         dial a transport to peer listening on multiaddr
         :param multiaddr: multiaddr of peer
-        :param self_id: peer_id of the dialer (to send to receier)
+        :param self_id: peer_id of the dialer (to send to receiver)
         :param options: optional object
         :return: True if successful
         """
