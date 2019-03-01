d9d359ceed953a5b4f725cf7f8b22e158e750886
Stuckinaboot <aspyn@seas.upenn.edu>
2019-03-01 05:37:09 -0500

    Add simple three node test

--- tests/pubsub/test_floodsub.py
+++ tests/pubsub/test_floodsub.py
@@ -42,11 +42,72 @@ async def test_simple_two_nodes():
 
     await asyncio.sleep(0.25)
 
-    f = await qb.get()
+    res_b = await qb.get()
 
     # Check that the msg received by node_b is the same
     # as the message sent by node_a
-    assert f == msg
+    assert res_b == msg
+
+    # Success, terminate pending tasks.
+    await cleanup()
+
+@pytest.mark.asyncio
+async def test_simple_three_nodes():
+    # Want to pass message from A -> B -> C
+    node_a = await new_node(transport_opt=["/ip4/127.0.0.1/tcp/0"])
+    node_b = await new_node(transport_opt=["/ip4/127.0.0.1/tcp/0"])
+    node_c = await new_node(transport_opt=["/ip4/127.0.0.1/tcp/0"])
+
+    supported_protocols = ["/floodsub/1.0.0"]
+
+    floodsub_a = FloodSub(supported_protocols)
+    pubsub_a = Pubsub(node_a, floodsub_a, "a")
+    floodsub_b = FloodSub(supported_protocols)
+    pubsub_b = Pubsub(node_b, floodsub_b, "b")
+    floodsub_c = FloodSub(supported_protocols)
+    pubsub_c = Pubsub(node_c, floodsub_c, "c")
+
+    # node_a connects to node_b
+    addr_b = node_b.get_addrs()[0]
+    info_b = info_from_p2p_addr(addr_b)
+    await node_a.connect(info_b)
+
+    # node_b connects to node_c
+    addr_c = node_c.get_addrs()[0]
+    info_c = info_from_p2p_addr(addr_c)
+    await node_b.connect(info_c)
+
+    await asyncio.sleep(0.25)
+    qb = await pubsub_b.subscribe("my_topic")
+    qc = await pubsub_c.subscribe("my_topic")
+    await asyncio.sleep(0.25)
+
+    node_a_id = str(node_a.get_id())
+
+    msg = "talk\n"
+    msg += node_a_id + '\n' + node_a_id + '\n'
+    msg += "my_topic" + '\n' + "some message data"
+
+    asyncio.ensure_future(floodsub_a.publish(node_a.get_id(), msg))
+    await asyncio.sleep(0.25)
+    res_b = await qb.get()
+    res_c = await qc.get()
+
+    # Check that the msg received by node_b is the same
+    # as the message sent by node_a
+    print("\n\n\n")
+    print(res_b)
+    print(msg)
+    print(res_b == msg)
+    assert res_b == msg
+
+    # res_c should match original msg but with b as sender
+    node_b_id = str(node_b.get_id())
+    msg_b_sender = "talk\n"
+    msg_b_sender += node_b_id + '\n' + node_a_id + '\n'
+    msg_b_sender += "my_topic" + '\n' + "some message data"
+
+    assert res_c == msg_b_sender
 
     # Success, terminate pending tasks.
     await cleanup()
