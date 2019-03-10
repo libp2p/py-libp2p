9345ce78b84aabd62a47e031bcad6f4823adf63e
Stuckinaboot <aspyn@seas.upenn.edu>
2019-03-10 19:29:02 +1000

    Add more complex seven node tree tests

--- tests/pubsub/test_dummyaccount_demo.py
+++ tests/pubsub/test_dummyaccount_demo.py
@@ -105,3 +105,35 @@ async def test_simple_seven_nodes_tree_topography():
         assert dummy_node.get_balance("aspyn") == 20
 
     await perform_test(num_nodes, adj_map, action_func, assertion_func)
+
+@pytest.mark.asyncio
+async def test_set_then_send_from_root_seven_nodes_tree_topography():
+    num_nodes = 7
+    adj_map = {0: [1, 2], 1: [3, 4], 2: [5, 6]}
+
+    async def action_func(dummy_nodes):
+        await dummy_nodes[0].publish_set_crypto("aspyn", 20)
+        await asyncio.sleep(0.25)
+        await dummy_nodes[0].publish_send_crypto("aspyn", "alex", 5)
+
+    def assertion_func(dummy_node):
+        assert dummy_node.get_balance("aspyn") == 15
+        assert dummy_node.get_balance("alex") == 5
+
+    await perform_test(num_nodes, adj_map, action_func, assertion_func)
+
+@pytest.mark.asyncio
+async def test_set_then_send_from_different_leafs_seven_nodes_tree_topography():
+    num_nodes = 7
+    adj_map = {0: [1, 2], 1: [3, 4], 2: [5, 6]}
+
+    async def action_func(dummy_nodes):
+        await dummy_nodes[6].publish_set_crypto("aspyn", 20)
+        await asyncio.sleep(0.25)
+        await dummy_nodes[4].publish_send_crypto("aspyn", "alex", 5)
+
+    def assertion_func(dummy_node):
+        assert dummy_node.get_balance("aspyn") == 15
+        assert dummy_node.get_balance("alex") == 5
+
+    await perform_test(num_nodes, adj_map, action_func, assertion_func)
