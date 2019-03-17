474602c4f72bc6fd6ba17b17ca7303bc08e47e9d
zixuanzh <zixuanzh@seas.upenn.edu>
2019-03-17 19:20:46 -0400

    refactor new_node

--- libp2p/__init__.py
+++ libp2p/__init__.py
@@ -11,6 +11,9 @@ from .transport.tcp.tcp import TCP
 
 
 async def cleanup_done_tasks():
+    """
+    clean up asyncio done tasks to free up resources
+    """
     while True:
         for task in asyncio.all_tasks():
             if task.done():
@@ -20,30 +23,59 @@ async def cleanup_done_tasks():
         # Some sleep necessary to context switch
         await asyncio.sleep(3)
 
-async def new_node(
+def initialize_default_swarm(
         id_opt=None, transport_opt=None,
-        muxer_opt=None, sec_opt=None, peerstore=None):
-
-    if id_opt is None:
+        muxer_opt=None, sec_opt=None, peerstore_opt=None):
+    """
+    initialize swarm when no swarm is passed in
+    :param id_opt: optional id for host
+    :param transport_opt: optional choice of transport upgrade
+    :param muxer_opt: optional choice of stream muxer
+    :param sec_opt: optional choice of security upgrade
+    :param peerstore_opt: optional peerstore
+    :return: return a default swarm instance
+    """
+    # pylint: disable=too-many-arguments
+    if not id_opt:
         new_key = RSA.generate(2048, e=65537)
         id_opt = id_from_public_key(new_key.publickey())
         # private_key = new_key.exportKey("PEM")
 
     transport_opt = transport_opt or ["/ip4/127.0.0.1/tcp/8001"]
-    transport_opt = [multiaddr.Multiaddr(t) for t in transport_opt]
-    muxer_opt = muxer_opt or ["mplex/6.7.0"]
-    sec_opt = sec_opt or ["secio"]
-    peerstore = peerstore or PeerStore()
-
-    upgrader = TransportUpgrader(sec_opt, transport_opt)
-    swarm = Swarm(id_opt, peerstore, upgrader)
+    transport = [multiaddr.Multiaddr(t) for t in transport_opt]
+    # TODO wire muxer up with swarm
+    # muxer = muxer_opt or ["mplex/6.7.0"]
+    sec = sec_opt or ["secio"]
+    peerstore = peerstore_opt or PeerStore()
+    upgrader = TransportUpgrader(sec, transport)
+    swarm_opt = Swarm(id_opt, peerstore, upgrader)
     tcp = TCP()
-    swarm.add_transport(tcp)
-    await swarm.listen(transport_opt[0])
+    swarm_opt.add_transport(tcp)
+
+    return swarm_opt
+
+async def new_node(
+        swarm_opt=None, id_opt=None, transport_opt=None,
+        muxer_opt=None, sec_opt=None, peerstore_opt=None):
+    """
+    create new libp2p node
+    :param id_opt: optional id for host
+    :param transport_opt: optional choice of transport upgrade
+    :param muxer_opt: optional choice of stream muxer
+    :param sec_opt: optional choice of security upgrade
+    :param peerstore_opt: optional peerstore
+    :return: return a default swarm instance
+    """
+    # pylint: disable=too-many-arguments
+    if not swarm_opt:
+        swarm_opt = initialize_default_swarm(
+            id_opt=id_opt, transport_opt=transport_opt,
+            muxer_opt=muxer_opt, sec_opt=sec_opt,
+            peerstore_opt=peerstore_opt)
 
     # TODO enable support for other host type
     # TODO routing unimplemented
-    host = BasicHost(swarm)
+    host = BasicHost(swarm_opt)
 
     # Kick off cleanup job
     asyncio.ensure_future(cleanup_done_tasks())
