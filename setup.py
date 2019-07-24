381f5ddc3ad5f65bca5e901a661d1d13034839cc
mhchia <kevin.mh.chia@gmail.com>
2019-07-24 18:43:49 +0800

    Replace `click` with `argparse`

--- setup.py
+++ setup.py
@@ -36,7 +36,6 @@ setuptools.setup(
     classifiers=classifiers,
     install_requires=[
         "pycryptodome>=3.8.2,<4.0.0",
-        "click>=7.0,<8.0",
         "base58>=1.0.3,<2.0.0",
         "pymultihash>=0.8.2",
         "multiaddr>=0.0.8,<0.1.0",
