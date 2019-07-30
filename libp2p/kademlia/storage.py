985b9ccf94993e2b2fc56451cd2644f8a0555d57
Chih Cheng Liang <chihchengliang@gmail.com>
2019-07-30 23:13:49 +0800

    add typing to storage

--- libp2p/kademlia/storage.py
+++ libp2p/kademlia/storage.py
@@ -3,6 +3,15 @@ from itertools import takewhile
 import operator
 from collections import OrderedDict
 from abc import abstractmethod, ABC
+from typing import (
+    TypeVar,
+    Iterator,
+    Tuple,
+    Any,
+)
+
+TKey = TypeVar('TKey', bound='IStorage')
+TValue = TypeVar('TValue', bound='IStorage')
 
 
 class IStorage(ABC):
@@ -12,82 +21,86 @@ class IStorage(ABC):
     """
 
     @abstractmethod
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: TKey, value: TValue) -> None:
         """
         Set a key to the given value.
         """
 
     @abstractmethod
-    def __getitem__(self, key):
+    def __getitem__(self, key: TKey) -> TValue:
         """
         Get the given key.  If item doesn't exist, raises C{KeyError}
         """
 
     @abstractmethod
-    def get(self, key, default=None):
+    def get(self, key: TKey, default: TValue = None)-> TValue:
         """
         Get given key.  If not found, return default.
         """
 
     @abstractmethod
-    def iter_older_than(self, seconds_old):
+    def iter_older_than(self, seconds_old: int)-> Iterator[Tuple[TKey, TValue]]:
         """
         Return the an iterator over (key, value) tuples for items older
         than the given seconds_old.
         """
 
     @abstractmethod
-    def __iter__(self):
+    def __iter__(self)-> Iterator[Tuple[TKey, TValue]]:
         """
         Get the iterator for this storage, should yield tuple of (key, value)
         """
 
 
 class ForgetfulStorage(IStorage):
-    def __init__(self, ttl=604800):
+    data: OrderedDict[Any, Any]
+    ttl: int
+
+    def __init__(self, ttl: int = 604800) -> None:
         """
         By default, max age is a week.
         """
         self.data = OrderedDict()
         self.ttl = ttl
 
-    def __setitem__(self, key, value):
+    def __setitem__(self, key: TKey, value: TValue) -> None:
         if key in self.data:
             del self.data[key]
         self.data[key] = (time.monotonic(), value)
         self.cull()
 
-    def cull(self):
+    def cull(self) -> None:
         for _, _ in self.iter_older_than(self.ttl):
             self.data.popitem(last=False)
 
-    def get(self, key, default=None):
+    def get(self, key: TKey, default: TValue = None) -> TValue:
         self.cull()
         if key in self.data:
             return self[key]
         return default
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: TKey) -> TValue:
         self.cull()
         return self.data[key][1]
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         self.cull()
         return repr(self.data)
 
-    def iter_older_than(self, seconds_old):
+    def iter_older_than(self, seconds_old: int) -> Iterator[Tuple[TKey, TValue]]:
         min_birthday = time.monotonic() - seconds_old
-        zipped = self._triple_iter()
+        zipped: Iterator[Tuple[TKey, int, TValue]] = self._triple_iter()
         matches = takewhile(lambda r: min_birthday >= r[1], zipped)
-        return list(map(operator.itemgetter(0, 2), matches))
+        for key, birthday, value in matches:
+            yield key, value
 
-    def _triple_iter(self):
+    def _triple_iter(self) -> Iterator[Tuple[TKey, int, TValue]]:
         ikeys = self.data.keys()
         ibirthday = map(operator.itemgetter(0), self.data.values())
         ivalues = map(operator.itemgetter(1), self.data.values())
         return zip(ikeys, ibirthday, ivalues)
 
-    def __iter__(self):
+    def __iter__(self) -> Iterator[Tuple[TKey, TValue]]:
         self.cull()
         ikeys = self.data.keys()
         ivalues = map(operator.itemgetter(1), self.data.values())
