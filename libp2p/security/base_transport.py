47d4cf6c4aea710849225fcb13d9c9901312e62c
Alex Stokes <r.alex.stokes@gmail.com>
2019-08-02 16:50:44 -0700

    Add an "insecure session" that satisfies the `ISecureConn` interface

---
+++ libp2p/security/base_transport.py
@@ -0,0 +1,13 @@
+from libp2p.peer.id import ID
+from libp2p.security.secure_transport_interface import ISecureTransport
+
+
+class BaseSecureTransport(ISecureTransport):
+    """
+    ``BaseSecureTransport`` is not fully instantiated from its abstract classes as it
+    is only meant to be used in clases that derive from it.
+    """
+
+    def __init__(self, local_private_key: bytes) -> None:
+        self.local_private_key = local_private_key
+        self.local_peer = ID.from_privkey(local_private_key)
