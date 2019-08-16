c03b19b0b8187d26861755b7a35ab35ccd33b3a1
mhchia <kevin.mh.chia@gmail.com>
2019-08-16 11:07:00 +0800

    Fix isort

--- libp2p/security/security_multistream.py
+++ libp2p/security/security_multistream.py
@@ -5,9 +5,9 @@ from libp2p.network.connection.raw_connection_interface import IRawConnection
 from libp2p.peer.id import ID
 from libp2p.protocol_muxer.multiselect import Multiselect
 from libp2p.protocol_muxer.multiselect_client import MultiselectClient
+from libp2p.protocol_muxer.multiselect_communicator import RawConnectionCommunicator
 from libp2p.security.secure_conn_interface import ISecureConn
 from libp2p.security.secure_transport_interface import ISecureTransport
-from libp2p.protocol_muxer.multiselect_communicator import RawConnectionCommunicator
 from libp2p.typing import TProtocol
 
 
