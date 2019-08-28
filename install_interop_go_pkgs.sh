15f62dff68f38d1e461ed8d450d12a90b7be8df9
mhchia <kevin.mh.chia@gmail.com>
2019-08-28 23:48:25 +0800

    Use go12 in CI

--- install_interop_go_pkgs.sh
+++ install_interop_go_pkgs.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-which go
-if [ $? != 0 ]; then
-    wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
-    sudo tar -C /usr/local -xzf $GOPACKAGE
-    export GOPATH=$HOME/go
-    export GOROOT=/usr/local/go
-fi
-
 go version
 cd tests/interop/go_pkgs/
 go install ./...
