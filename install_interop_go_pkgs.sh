5280f3965c001ce65d8a7aaa66d0abe68732928e
mhchia <kevin.mh.chia@gmail.com>
2019-09-03 17:41:17 +0800

    Update install script for interop
    And adjust the structure of go packages for interop

--- install_interop_go_pkgs.sh
+++ install_interop_go_pkgs.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+GO_PKGS_PATH=./tests/interop/go_pkgs
+
+DAEMON_REPO=go-libp2p-daemon
+DAEMON_PATH=$GO_PKGS_PATH/$DAEMON_REPO
+
+EXAMPLES_PATHS=$GO_PKGS_PATH/examples
+
+
 go version
-cd tests/interop/go_pkgs/
+
+# Install `p2pd`
+# FIXME: Use the canonical repo in libp2p, when we don't need `insecure`.
+if [ ! -e "$DAEMON_PATH" ]; then
+    git clone git@github.com:mhchia/$DAEMON_REPO.git --branch test/add-options $DAEMON_PATH
+fi
+cd $DAEMON_PATH
 go install ./...
+
+cd -
+
+# Install example modeuls
+cd $EXAMPLES_PATHS
+go install ./...
+
+echo "Finish installing go modeuls for interop."
