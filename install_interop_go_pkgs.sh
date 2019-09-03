c6d81d70b3d155765d5280f811353b0de7c4297a
mhchia <kevin.mh.chia@gmail.com>
2019-09-03 22:25:29 +0800

    Try 3.7-dev in CI
    Use https over ssh when cloning go-libp2p-daemon
    
    To avoid """
    Warning: Permanently added the RSA host key for IP address '140.82.113.4' to the list of known hosts.
    Permission denied (publickey).
    fatal: Could not read from remote repository.
    """
    
    Exit if clone fails
    
    Fix daemon url

--- install_interop_go_pkgs.sh
+++ install_interop_go_pkgs.sh
@@ -13,15 +13,18 @@ go version
 # Install `p2pd`
 # FIXME: Use the canonical repo in libp2p, when we don't need `insecure`.
 if [ ! -e "$DAEMON_PATH" ]; then
-    git clone git@github.com:mhchia/$DAEMON_REPO.git --branch test/add-options $DAEMON_PATH
+    git clone https://github.com/mhchia/$DAEMON_REPO.git --branch test/add-options $DAEMON_PATH
+    if [ "$?" != 0 ]; then
+        echo "Failed to clone the daemon repo"
+        exit 1
+    fi
 fi
-cd $DAEMON_PATH
-go install ./...
+
+cd $DAEMON_PATH && go install ./...
 
 cd -
 
 # Install example modeuls
-cd $EXAMPLES_PATHS
-go install ./...
+cd $EXAMPLES_PATHS && go install ./...
 
 echo "Finish installing go modeuls for interop."
