9837f306981037a9824e850235ead5d0c1bbcc71
NIC619 <twedusuck@gmail.com>
2019-11-24 16:52:46 +0800

    Rename `peer_map` to `peer_data_map`

--- libp2p/peer/peerstore.py
+++ libp2p/peer/peerstore.py
@@ -10,35 +10,35 @@ from .peerstore_interface import IPeerStore
 
 class PeerStore(IPeerStore):
 
-    peer_map: Dict[ID, PeerData]
+    peer_data_map: Dict[ID, PeerData]
 
     def __init__(self) -> None:
         IPeerStore.__init__(self)
-        self.peer_map = {}
+        self.peer_data_map = {}
 
     def __create_or_get_peer(self, peer_id: ID) -> PeerData:
         """
         Returns the peer data for peer_id or creates a new peer data (and
-        stores it in peer_map) if peer data for peer_id does not yet exist.
+        stores it in peer_data_map) if peer data for peer_id does not yet exist.
 
         :param peer_id: peer ID
         :return: peer data
         """
-        if peer_id in self.peer_map:
-            return self.peer_map[peer_id]
+        if peer_id in self.peer_data_map:
+            return self.peer_data_map[peer_id]
         data = PeerData()
-        self.peer_map[peer_id] = data
-        return self.peer_map[peer_id]
+        self.peer_data_map[peer_id] = data
+        return self.peer_data_map[peer_id]
 
     def peer_info(self, peer_id: ID) -> Optional[PeerInfo]:
-        if peer_id in self.peer_map:
-            peer_data = self.peer_map[peer_id]
+        if peer_id in self.peer_data_map:
+            peer_data = self.peer_data_map[peer_id]
             return PeerInfo(peer_id, peer_data.addrs)
         return None
 
     def get_protocols(self, peer_id: ID) -> List[str]:
-        if peer_id in self.peer_map:
-            return self.peer_map[peer_id].get_protocols()
+        if peer_id in self.peer_data_map:
+            return self.peer_data_map[peer_id].get_protocols()
         raise PeerStoreError("peer ID not found")
 
     def add_protocols(self, peer_id: ID, protocols: Sequence[str]) -> None:
@@ -50,12 +50,12 @@ class PeerStore(IPeerStore):
         peer.set_protocols(list(protocols))
 
     def peer_ids(self) -> List[ID]:
-        return list(self.peer_map.keys())
+        return list(self.peer_data_map.keys())
 
     def get(self, peer_id: ID, key: str) -> Any:
-        if peer_id in self.peer_map:
+        if peer_id in self.peer_data_map:
             try:
-                val = self.peer_map[peer_id].get_metadata(key)
+                val = self.peer_data_map[peer_id].get_metadata(key)
             except PeerDataError as error:
                 raise PeerStoreError(error)
             return val
@@ -76,21 +76,21 @@ class PeerStore(IPeerStore):
         peer.add_addrs(list(addrs))
 
     def addrs(self, peer_id: ID) -> List[Multiaddr]:
-        if peer_id in self.peer_map:
-            return self.peer_map[peer_id].get_addrs()
+        if peer_id in self.peer_data_map:
+            return self.peer_data_map[peer_id].get_addrs()
         raise PeerStoreError("peer ID not found")
 
     def clear_addrs(self, peer_id: ID) -> None:
         # Only clear addresses if the peer is in peer map
-        if peer_id in self.peer_map:
-            self.peer_map[peer_id].clear_addrs()
+        if peer_id in self.peer_data_map:
+            self.peer_data_map[peer_id].clear_addrs()
 
     def peers_with_addrs(self) -> List[ID]:
         # Add all peers with addrs at least 1 to output
         output: List[ID] = []
 
-        for peer_id in self.peer_map:
-            if len(self.peer_map[peer_id].get_addrs()) >= 1:
+        for peer_id in self.peer_data_map:
+            if len(self.peer_data_map[peer_id].get_addrs()) >= 1:
                 output.append(peer_id)
         return output
 
