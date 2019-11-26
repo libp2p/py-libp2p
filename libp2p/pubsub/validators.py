f3c997215919c512cade5ea427e80b8207e3365c
NIC619 <twedusuck@gmail.com>
2019-11-26 16:10:20 +0800

    Implement Pubsub signature validator

--- libp2p/pubsub/validators.py
+++ libp2p/pubsub/validators.py
@@ -1,10 +1,17 @@
-# FIXME: Replace the type of `pubkey` with a custom type `Pubkey`
-def signature_validator(pubkey: bytes, msg: bytes) -> bool:
+from libp2p.crypto.keys import PublicKey
+
+from .pb import rpc_pb2
+
+
+def signature_validator(pubkey: PublicKey, msg: rpc_pb2.Message) -> bool:
     """
     Verify the message against the given public key.
 
     :param pubkey: the public key which signs the message.
     :param msg: the message signed.
     """
-    # TODO: Implement the signature validation
+    try:
+        pubkey.verify(msg.SerializeToString(), msg.signature)
+    except Exception:
+        return False
     return True
