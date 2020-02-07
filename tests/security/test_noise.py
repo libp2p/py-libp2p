23ece34157ec148897ccf6f01adb7b24f0f1cd8a
mhchia <kevin.mh.chia@gmail.com>
2020-02-07 18:17:15 +0800

    test_noise_connection: fix missing nursery

--- tests/security/test_noise.py
+++ tests/security/test_noise.py
@@ -3,6 +3,9 @@ import pytest
 from libp2p.tools.factories import noise_conn_factory
 
 
+DATA = b"testing_123"
+
+
 @pytest.mark.trio
 async def test_noise_transport(nursery):
     async with noise_conn_factory(nursery):
@@ -10,6 +13,9 @@ async def test_noise_transport(nursery):
 
 
 @pytest.mark.trio
-async def test_noise_connection():
-    async with noise_conn_factory() as conns:
+async def test_noise_connection(nursery):
+    async with noise_conn_factory(nursery) as conns:
         local_conn, remote_conn = conns
+        await local_conn.write(DATA)
+        read_data = await remote_conn.read(len(DATA))
+        assert read_data == DATA
