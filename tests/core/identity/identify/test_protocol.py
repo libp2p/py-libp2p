bd8bd953ec250baccb212791751dc130cc07e668
acul71 <luca.pisani@birdo.net>
2025-02-19 03:43:50 +0100

    feat: add agent version to identify protocol and improved tests

--- tests/core/identity/identify/test_protocol.py
+++ tests/core/identity/identify/test_protocol.py
@@ -4,8 +4,11 @@ from libp2p.identity.identify.pb.identify_pb2 import (
     Identify,
 )
 from libp2p.identity.identify.protocol import (
+    AGENT_VERSION,
     ID,
+    PROTOCOL_VERSION,
     _mk_identify_protobuf,
+    _multiaddr_to_bytes,
 )
 from tests.factories import (
     host_pair_factory,
@@ -24,4 +27,26 @@ async def test_identify_protocol(security_protocol):
 
         identify_response = Identify()
         identify_response.ParseFromString(response)
+
+        # sanity check
         assert identify_response == _mk_identify_protobuf(host_a)
+
+        # Check protocol version
+        assert identify_response.protocol_version == PROTOCOL_VERSION
+
+        # Check agent version
+        assert identify_response.agent_version == AGENT_VERSION
+
+        # Check public key
+        assert identify_response.public_key == host_a.get_public_key().serialize()
+
+        # Check listen addresses
+        assert identify_response.listen_addrs == list(
+            map(_multiaddr_to_bytes, host_a.get_addrs())
+        )
+
+        # TODO: Check observed address
+        # assert identify_response.observed_addr == host_b.get_addrs()[0]
+
+        # Check protocols
+        assert set(identify_response.protocols) == set(host_a.get_mux().get_protocols())
