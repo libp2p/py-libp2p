cf471082d0dfee4b6f64f2d78d9f1409b381f8e9
Paschal <58183764+paschal533@users.noreply.github.com>
2025-02-23 12:47:00 +0100

    feat: Add Windows compatibility using coincurve

--- libp2p/crypto/key_exchange.py
+++ libp2p/crypto/key_exchange.py
@@ -1,11 +1,26 @@
+import sys
 from typing import (
     Callable,
     cast,
 )
 
-from fastecdsa.encoding import (
-    util,
-)
+if sys.platform != "win32":
+    from fastecdsa.encoding import (
+        util,
+    )
+
+    int_bytelen = util.int_bytelen
+else:
+    from math import (
+        ceil,
+        log2,
+    )
+
+    def int_bytelen(n: int) -> int:
+        if n == 0:
+            return 1
+        return ceil(log2(abs(n) + 1) / 8)
+
 
 from libp2p.crypto.ecc import (
     ECCPrivateKey,
@@ -17,8 +32,6 @@ from libp2p.crypto.keys import (
 )
 
 SharedKeyGenerator = Callable[[bytes], bytes]
-
-int_bytelen = util.int_bytelen
 
 
 def create_ephemeral_key_pair(curve_type: str) -> tuple[PublicKey, SharedKeyGenerator]:
@@ -32,9 +45,15 @@ def create_ephemeral_key_pair(curve_type: str) -> tuple[PublicKey, SharedKeyGene
         private_key = cast(ECCPrivateKey, key_pair.private_key)
 
         remote_point = ECCPublicKey.from_bytes(serialized_remote_public_key, curve_type)
-        secret_point = remote_point.impl * private_key.impl
-        secret_x_coordinate = secret_point.x
-        byte_size = int_bytelen(secret_x_coordinate)
-        return secret_x_coordinate.to_bytes(byte_size, byteorder="big")
+
+        if sys.platform != "win32":
+            secret_point = remote_point.impl * private_key.impl
+            secret_x_coordinate = secret_point.x
+            byte_size = int_bytelen(secret_x_coordinate)
+            return secret_x_coordinate.to_bytes(byte_size, byteorder="big")
+        else:
+            # Windows implementation using coincurve
+            shared_key = private_key.impl.ecdh(remote_point.impl.public_key)
+            return shared_key
 
     return key_pair.public_key, _key_exchange
