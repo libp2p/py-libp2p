5fe061334b265d2dc686949c054a9ad35e84c0fa
paschal533 <okwuosahpaschal@gmail.com>
2025-03-28 11:22:54 +0100

    Fix: merge conflict in docs/libp2p.tools.rst libp2p/tools and test

--- tests/utils/pubsub/dummy_account_node.py
+++ tests/utils/pubsub/dummy_account_node.py
@@ -12,11 +12,13 @@ from libp2p.abc import (
 from libp2p.pubsub.pubsub import (
     Pubsub,
 )
-from libp2p.tools.anyio_service import (
+from libp2p.tools.async_service import (
     Service,
-    background_anyio_service,
+    background_trio_service,
 )
-from tests.factories import pubsub_batch_with_floodsub  # Updated import
+from tests.utils.factories import (
+    PubsubFactory,
+)
 
 CRYPTO_TOPIC = "ethereum"
 
@@ -62,12 +64,12 @@ class DummyAccountNode(Service):
         We use create as this serves as a factory function and allows us
         to use async await, unlike the init function
         """
-        async with pubsub_batch_with_floodsub(number) as pubsubs:  # Updated call
+        async with PubsubFactory.create_batch_with_floodsub(number) as pubsubs:
             async with AsyncExitStack() as stack:
-                dummy_account_nodes = tuple(cls(pubsub) for pubsub in pubsubs)
-                for node in dummy_account_nodes:
-                    await stack.enter_async_context(background_anyio_service(node))
-                yield dummy_account_nodes
+                dummy_acount_nodes = tuple(cls(pubsub) for pubsub in pubsubs)
+                for node in dummy_acount_nodes:
+                    await stack.enter_async_context(background_trio_service(node))
+                yield dummy_acount_nodes
 
     async def handle_incoming_msgs(self) -> None:
         """Handle all incoming messages on the CRYPTO_TOPIC from peers."""
