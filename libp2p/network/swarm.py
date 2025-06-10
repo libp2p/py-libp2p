59a7ee0656f8a88ab786b3fafe5059f8c514e08f
lla-dane <abhinavagarwalla6@gmail.com>
2025-06-10 15:10:39 +0530

    removed extar logs

--- libp2p/network/swarm.py
+++ libp2p/network/swarm.py
@@ -127,7 +127,6 @@ class Swarm(Service, INetworkService):
         :return: muxed connection
         """
         if peer_id in self.connections:
-            logger.info("WE ARE RETURNING, PEER ALREADAY EXISTS")
             # If muxed connection already exists for peer_id,
             # set muxed connection equal to existing muxed connection
             return self.connections[peer_id]
@@ -148,7 +147,6 @@ class Swarm(Service, INetworkService):
         # Try all known addresses
         for multiaddr in addrs:
             try:
-                logger.info("HANDSHAKE GOING TO HAPPEN")
                 return await self.dial_addr(multiaddr, peer_id)
             except SwarmException as e:
                 exceptions.append(e)
@@ -223,10 +221,8 @@ class Swarm(Service, INetworkService):
         logger.debug("attempting to open a stream to peer %s", peer_id)
 
         swarm_conn = await self.dial_peer(peer_id)
-        logger.info("INETCONN CREATED")
 
         net_stream = await swarm_conn.new_stream()
-        logger.info("INETSTREAM CREATED")
 
         logger.debug("successfully opened a stream to peer %s", peer_id)
         return net_stream
