9a1d2de834eaf3ff75f3db478c59d1da4524a928
lla-dane <abhinavagarwalla6@gmail.com>
2026-01-18 12:06:34 +0530

    prioritize NOISE over TLS

--- libp2p/__init__.py
+++ libp2p/__init__.py
@@ -301,13 +301,12 @@ def new_swarm(
     # with mutual TLS authentication. See TLS_ANALYSIS.md for details.
     # TLS is still offered as a fallback option.
     secure_transports_by_protocol: Mapping[TProtocol, ISecureTransport] = sec_opt or {
-        TLS_PROTOCOL_ID: TLSTransport (
-            key_pair
-        ),
         NOISE_PROTOCOL_ID: NoiseTransport(
             key_pair, noise_privkey=noise_key_pair.private_key
         ),
-
+        TLS_PROTOCOL_ID: TLSTransport (
+            key_pair
+        ),
         TProtocol(secio.ID): secio.Transport(key_pair),
         TProtocol(PLAINTEXT_PROTOCOL_ID): InsecureTransport(
             key_pair, peerstore=peerstore_opt
