2fe79044f31c6a52b387dc801e74ddf26c1f2e51
asmit27rai <raiasmit10@gmail.com>
2026-03-22 03:49:16 +0530

    lint

--- libp2p/kad_dht/grid_routing_table.py
+++ libp2p/kad_dht/grid_routing_table.py
@@ -51,7 +51,7 @@ class NodeId:
     @classmethod
     def from_hash(cls, hash_data: bytes) -> "NodeId":
         """Create a NodeId from a pre-computed hash."""
-        node_id = cls.__new__(cls)
+        node_id: NodeId = cls.__new__(cls)  # type: ignore[assignment]
         node_id.peer_id = None
         node_id.data = hash_data
         return node_id
