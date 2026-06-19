b6e6a50b58cfbfee009d455c92baae86701f2dfc
sumanjeet0012 <sumanjeet0012@gmail.com>
2026-06-19 11:23:13 +0530

    feat: expose public API in __init__.py

---
+++ py_ipfs_lite/__init__.py
@@ -0,0 +1,15 @@
+from py_ipfs_lite.peer import Peer
+from py_ipfs_lite.config import Config, AddParams
+from py_ipfs_lite.interfaces import Host, Routing, BlockStore, Exchange, DagService
+
+__all__ = [
+    "Peer",
+    "Config",
+    "AddParams",
+    "Host",
+    "Routing",
+    "BlockStore",
+    "Exchange",
+    "DagService",
+]
+__version__ = "0.1.0"
