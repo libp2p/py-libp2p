cf58c946418cd91f377e182c18666b28aac89e9e
sumanjeet0012 <sumanjeet0012@gmail.com>
2026-08-01 01:28:36 +0530

    chore: remove scratch test files

--- test5.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(level=logging.INFO)
-from libp2p.network.auto_connector import logger
-
-print("IS INFO ENABLED?", logger.isEnabledFor(logging.INFO))
-logger.info("TEST INFO")
-logger.warning("TEST WARNING")
-logger.error("TEST ERROR")
